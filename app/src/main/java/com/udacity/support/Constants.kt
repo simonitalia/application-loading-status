0c45f3af3f950dee456921682251babd1fb7f606
Simon Italia <sitalia@magicaltomato.com>
2021-09-03 16:13:01 +1000

    new constants file and values

---
+++ app/src/main/java/com/udacity/support/Constants.kt
@@ -0,0 +1,10 @@
+package com.udacity.support
+
+object Constants {
+
+    const val GH_BASE_URL = "https://github.com/"
+    const val GLIDE_REPO_URL = "glide/archive/refs/heads/master.zip"
+    const val ADVANCED_PROGRAMMING_REPO_URL = "udacity/nd940-c3-advanced-android-programming-project-starter/archive/master.zip"
+    const val RETROFIT_REPO_URL = "square/retrofit/archive/refs/heads/master.zip"
+    const val CHANNEL_ID = "channelId"
+}
