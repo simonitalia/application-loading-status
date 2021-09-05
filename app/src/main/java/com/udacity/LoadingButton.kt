32e23c4013a9da0b5298b8e2d918deb6f9e7d9ce
Simon Italia <sitalia@magicaltomato.com>
2021-09-05 21:08:51 +1000

    configure custom button text

--- app/src/main/java/com/udacity/LoadingButton.kt
+++ app/src/main/java/com/udacity/LoadingButton.kt
@@ -4,38 +4,41 @@ import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import android.util.Log
 import android.view.View
 import androidx.core.content.withStyledAttributes
-import kotlin.properties.Delegates
+import kotlin.math.roundToInt
 
 
-sealed class ButtonStates {
-    object Clicked : ButtonStates()
-    object Loading : ButtonStates()
-    object Completed : ButtonStates()
-}
+//sealed class ButtonStates {
+//    object Clicked : ButtonStates()
+//    object Loading : ButtonStates()
+//    object Completed : ButtonStates()
+//}
 
 
 class LoadingButton @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
 
-    private enum class ButtonState {
-        NORMAL,
-        DOWNLOADING
+    private enum class ButtonState(val text: Int) {
+        NORMAL(text = R.string.button_normal_text),
+        DOWNLOADING(text = R.string.button_downloading_text)
     }
 
-    //Paint object
-    private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+    //Paint text object
+    private val paintText = Paint(Paint.ANTI_ALIAS_FLAG).apply {
         style = Paint.Style.FILL
         textAlign = Paint.Align.CENTER
-        textSize = 100.0f
+        color = Color.WHITE
+        textSize = 50.0f
         typeface = Typeface.create( "", Typeface.BOLD)
     }
 
-    private var normalStateColor = 0
-    private var downloadingStateColor = 0
+    private var normalStateColor: Int = 0
+    private var downloadingStateColor: Int = 0
 
+    // canvas (view) dimensions
     private var viewWidth = 0
     private var viewHeight = 0
 
@@ -54,20 +57,32 @@ class LoadingButton @JvmOverloads constructor(
             normalStateColor = getColor(R.styleable.LoadingButton_buttonColorPrimary, 0)
             downloadingStateColor = getColor(R.styleable.LoadingButton_buttonColorPrimaryDark, 0)
         }
+
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
 
-        paint.color = when (buttonState) {
-            ButtonState.NORMAL -> normalStateColor
-            ButtonState.DOWNLOADING -> downloadingStateColor
+       // get text position
+        val viewCenterX = (width / 2).toFloat()  // canvas (view) position
+        val textPositionY = (height * 0.6f).roundToInt().toFloat() // canvas (view) text drawing position
 
+        var buttonText = ""
+
+        // set button color & text based on button state
+         when (buttonState) {
+            ButtonState.NORMAL -> {
+                setBackgroundColor(normalStateColor)
+                buttonText = resources.getString(ButtonState.NORMAL.text)
+            }
+
+            ButtonState.DOWNLOADING -> {
+                setBackgroundColor(downloadingStateColor)
+                buttonText = resources.getString(ButtonState.DOWNLOADING.text)
+            }
         }
 
-        val rect = Rect()
-        canvas?.drawRect(rect, paint)
-
+        canvas?.drawText(buttonText, viewCenterX, textPositionY, paintText)
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
@@ -78,29 +93,10 @@ class LoadingButton @JvmOverloads constructor(
             heightMeasureSpec,
             0
         )
+
         viewWidth = w
         viewHeight = h
-
         setMeasuredDimension(w, h)
-    }
-
-    private fun measureWidth(measureSpec: Int): Int {
-        val specMode = MeasureSpec.getMode(measureSpec)
-        val specSize = MeasureSpec.getSize(measureSpec)
-        return if (specMode == MeasureSpec.EXACTLY) {
-            specSize
-        } else {
-            viewWidth
-        }
-    }
-
-    private fun measureHeight(measureSpecHeight: Int): Int {
-        val specMode = MeasureSpec.getMode(measureSpecHeight)
-        val specSize = MeasureSpec.getSize(measureSpecHeight)
-        return if (specMode == MeasureSpec.EXACTLY) {
-            specSize
-        } else {
-            viewHeight
-        }
+        Log.i("LoadingButton.init", "Canvas Dimens: w: $width, h: $height")
     }
 }
