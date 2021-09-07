015ed77c216894cf559fc89d03c041b67a20a4e6
Simon Italia <sitalia@magicaltomato.com>
2021-09-07 11:00:57 +1000

    implement download animation logic

--- app/src/main/java/com/udacity/LoadingButton.kt
+++ app/src/main/java/com/udacity/LoadingButton.kt
@@ -1,5 +1,6 @@
 package com.udacity
 
+import android.animation.PropertyValuesHolder
 import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.*
@@ -8,33 +9,43 @@ import android.util.Log
 import android.view.View
 import androidx.core.content.withStyledAttributes
 import kotlin.math.roundToInt
-
-
-//sealed class ButtonStates {
-//    object Clicked : ButtonStates()
-//    object Loading : ButtonStates()
-//    object Completed : ButtonStates()
-//}
+import kotlin.properties.Delegates
 
 
 class LoadingButton @JvmOverloads constructor(
-    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
-) : View(context, attrs, defStyleAttr) {
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+): View(context, attrs, defStyleAttr) {
 
-    private enum class ButtonState(val text: Int) {
+    companion object {
+        const val PERCENTAGE_DIVIDER = 100.0
+        const val PERCENTAGE_VALUE_HOLDER = "percentage"
+    }
+
+    private var currentPercentage = 0
+
+    enum class ButtonState(val text: Int) {
         NORMAL(text = R.string.button_normal_text),
         DOWNLOADING(text = R.string.button_downloading_text)
     }
 
-    //Paint text object
-    private val paintText = Paint(Paint.ANTI_ALIAS_FLAG).apply {
-        style = Paint.Style.FILL
-        textAlign = Paint.Align.CENTER
-        color = Color.WHITE
-        textSize = 50.0f
-        typeface = Typeface.create( "", Typeface.BOLD)
+    private val animator = ValueAnimator.ofFloat(0f, -100f)
+    private var isDownloading = false
+
+    // Button State observable
+    var buttonState: ButtonState by Delegates.observable(ButtonState.NORMAL) { _, _, _ ->
+        isClickable = buttonState != ButtonState.DOWNLOADING
+        isDownloading = buttonState == ButtonState.DOWNLOADING
+
+        if (buttonState == ButtonState.DOWNLOADING){
+            animateProgress()
+        }
+
+        invalidate() //force redraw of view
     }
 
+    // storage for retrieved styling attribute colors
     private var normalStateColor: Int = 0
     private var downloadingStateColor: Int = 0
 
@@ -42,49 +53,29 @@ class LoadingButton @JvmOverloads constructor(
     private var viewWidth = 0
     private var viewHeight = 0
 
-    private var buttonState = ButtonState.NORMAL
+    // button container object
+    private val viewContainer = RectF()
 
-    private val valueAnimator = ValueAnimator()
+    private var downloadingRect = RectF()
 
-//    private var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) { p, old, new ->
-//
-//    }
 
     init {
-        isClickable = true // enable user interaction with button
-
         context.withStyledAttributes(attrs, R.styleable.LoadingButton) {
-            normalStateColor = getColor(R.styleable.LoadingButton_buttonColorPrimary, 0)
-            downloadingStateColor = getColor(R.styleable.LoadingButton_buttonColorPrimaryDark, 0)
+            normalStateColor = getColor(R.styleable.LoadingButton_backgroundColor, 0)
+            downloadingStateColor = getColor(R.styleable.LoadingButton_secondaryBackgroundColor, 0)
         }
-
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
 
-       // get text position
-        val viewCenterX = (width / 2).toFloat()  // canvas (view) position
-        val textPositionY = (height * 0.6f).roundToInt().toFloat() // canvas (view) text drawing position
+        drawNormalStateView(canvas)
+        if (buttonState == ButtonState.DOWNLOADING) { drawDownloadingStateView(canvas) }
 
-        var buttonText = ""
-
-        // set button color & text based on button state
-         when (buttonState) {
-            ButtonState.NORMAL -> {
-                setBackgroundColor(normalStateColor)
-                buttonText = resources.getString(ButtonState.NORMAL.text)
-            }
-
-            ButtonState.DOWNLOADING -> {
-                setBackgroundColor(downloadingStateColor)
-                buttonText = resources.getString(ButtonState.DOWNLOADING.text)
-            }
-        }
-
-        canvas?.drawText(buttonText, viewCenterX, textPositionY, paintText)
+        drawViewText(canvas)
     }
 
+    // set canvas view space
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         val minw: Int = paddingLeft + paddingRight + suggestedMinimumWidth
         val w: Int = resolveSizeAndState(minw, widthMeasureSpec, 1)
@@ -97,6 +88,89 @@ class LoadingButton @JvmOverloads constructor(
         viewWidth = w
         viewHeight = h
         setMeasuredDimension(w, h)
-        Log.i("LoadingButton.init", "Canvas Dimens: w: $width, h: $height")
+
+        //set viewContainer space
+        viewContainer.set(
+            x,
+            y,
+            x + w,
+            y + h
+        )
+
+        Log.i("LoadingButton.init", "Canvas Dimens: w: $viewWidth, h: $viewHeight")
+    }
+
+    private fun drawNormalStateView(canvas: Canvas?) {
+        val viewContainerPaint = Paint().apply {
+            style = Paint.Style.FILL
+            isAntiAlias = true
+            color = context.getColor(R.color.primary_green)
+        }
+
+        canvas?.drawRect(viewContainer, viewContainerPaint)
+    }
+
+    private fun drawDownloadingStateView(canvas: Canvas?) {
+
+        fun getCurrentPercentageToFill() =
+            (viewWidth * (currentPercentage / PERCENTAGE_DIVIDER)).toFloat()
+
+        val paint = Paint().apply {
+            style = Paint.Style.FILL
+            isAntiAlias = true
+            color = context.getColor(R.color.primary_dark_green)
+        }
+
+        val percentageToFill = getCurrentPercentageToFill()
+
+        downloadingRect.set(
+            0f,
+            0f,
+            0f + percentageToFill,
+            0f + (viewHeight)
+        )
+
+        canvas?.drawRect(downloadingRect, paint)
+    }
+
+    private fun animateProgress() {
+        val valuesHolder = PropertyValuesHolder.ofFloat("percentage", 0f, 100f)
+        val animator = ValueAnimator().apply {
+            setValues(valuesHolder)
+            duration = 1000
+            addUpdateListener {
+                val percentage = it.getAnimatedValue(PERCENTAGE_VALUE_HOLDER) as Float
+                currentPercentage = percentage.toInt()
+                invalidate()
+            }
+        }
+
+       animator.start()
+    }
+
+    private fun drawViewText(canvas: Canvas?) {
+
+        // paint object for drawing button text
+        val paintText = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+            style = Paint.Style.FILL
+            textAlign = Paint.Align.CENTER
+            color = Color.WHITE
+            textSize = 50.0f
+            typeface = Typeface.create( "", Typeface.BOLD)
+        }
+
+        // set x, y text drawing position
+        val viewCenterX = (width / 2).toFloat()  // canvas (view) center x
+        val textPositionY = (height * 0.6f).roundToInt().toFloat() // canvas (view) y position
+
+        // draw text
+        val buttonText = if (buttonState == ButtonState.NORMAL) {
+            resources.getString(ButtonState.NORMAL.text)
+
+        } else {
+            resources.getString(ButtonState.DOWNLOADING.text)
+        }
+
+        canvas?.drawText(buttonText, viewCenterX, textPositionY, paintText)
     }
 }
